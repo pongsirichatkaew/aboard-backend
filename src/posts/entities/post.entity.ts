6dff5388e1394096ee7f44f9cd74463ad96babbc
Pongsiri chatkaew <pongsiri.cha@hotmail.com>
2024-11-26 13:28:04 +0700

    feat: post entity with module

---
+++ src/posts/entities/post.entity.ts
@@ -0,0 +1,31 @@
+import { User } from 'src/users/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  community: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @ManyToOne(() => User)
+  @JoinColumn()
+  user: User;
+}
---
+++ src/posts/enums/posts.enums.ts
@@ -0,0 +1,8 @@
+export enum PostCommunity {
+  History = 'History',
+  Pets = 'Pets',
+  Health = 'Health',
+  Fashion = 'Fashion',
+  Exercise = 'Exercise',
+  Others = 'Others',
+}
