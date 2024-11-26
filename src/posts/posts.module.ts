b1ccb31f0b30ae0a5e75443bbe5fd82b912b39a3
Pongsiri chatkaew <pongsiri.cha@hotmail.com>
2024-11-26 15:25:29 +0700

    feat: import module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { PostComment } from './entities/post-comment.entity';
 import { Post } from './entities/post.entity';
 import { PostsController } from './posts.controller';
-import { PostsService } from './posts.service';
+import { PostCommentService } from './services/post-comment.service';
+import { PostsService } from './services/posts.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post]), AuthModule],
+  imports: [TypeOrmModule.forFeature([Post, PostComment]), AuthModule],
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [PostsService, PostCommentService],
 })
 export class PostsModule {}
